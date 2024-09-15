dd468ebbd2f9a146d0adc15a34294d067a371b61
Mimar Aslan <mimaraslan@gmail.com>
2024-09-15 12:44:51 +0300

    DevOps Pipeline
    DevOps Pipeline

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,15 @@ FROM openjdk:17
 # Projemizin Jar dosyasının konumu
 ARG JAR_FILE=target/*.jar
 
-#  Projenin Jar halini Docker'ın içine şu isimle kopayala
-COPY ${JAR_FILE} devops-hello-app.jar
+#  Projenin Jar halini Docker'ın içine şu isimle kopyala
+COPY ${JAR_FILE} my-application.jar
 
 # terminalden çalıştırmak istediğin komutları CMD ile kullaniyorsunuz.
 CMD apt-get update
 CMD apt-get upgrade -y
 
 # iç portu sabitlemek için 
-EXPOSE 8080
+EXPOSE 8081
 
 # Uygulamanın çalışacağı komut
-ENTRYPOINT ["java","-jar","devops-hello-app.jar"]
+ENTRYPOINT ["java","-jar","my-application.jar"]
--- src/main/java/com/mimaraslan/controller/DevOpsController.java
+++ src/main/java/com/mimaraslan/controller/DevOpsController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 
-//  http://localhost:8080
+//  http://localhost:8081
 @RestController
 @RequestMapping
 public class DevOpsController {
 
-    //  http://localhost:8080
+    //  http://localhost:8081
     @GetMapping
     public String devopsHello() {
         return "DevOps Hello : " + LocalDateTime.now();
     }
 
-    //  http://localhost:8080/info
+    //  http://localhost:8081/info
     @GetMapping("/info")
     public String info() {
         return "DEVOPS INFO : " + LocalDateTime.now();
